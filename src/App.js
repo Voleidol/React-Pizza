85a93b669cda067cbf1683a4b469b818070c3c5e
Роман Кривошеев <krivosheev@amz.lan>
2022-09-21 15:35:12 +0300

    add free lesson

--- src/App.js
+++ src/App.js
@@ -7,20 +7,22 @@ import './scss/app.scss';
 
 function App() {
   return (
-    <div class="wrapper">
+    <div className="wrapper">
       <Header />
-      <div class="content">
-        <div class="container">
-          <div class="content__top">
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
             <Categories />
             <Sort />
           </div>
-          <h2 class="content__title">Все пиццы</h2>
-          <div class="content__items">
-            <PizzaBlock title="Мексиканская" price="500"/>
-            <PizzaBlock title="Мексиканская" price="600"/>
-            <PizzaBlock title="Мексиканская" price="700"/>
-            <PizzaBlock title="Мексиканская" price="850"/>          
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Мексиканская" price={500}/>
+            <PizzaBlock title="Мексиканская" price={350}/>           
+            {/* {PizzaBlock({
+              title: "Мексиканская",
+              price: "500"
+            })}        Тоже самое, просто передаёт объект   */}
           </div>
         </div>
       </div>
